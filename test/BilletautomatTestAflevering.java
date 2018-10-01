2d1f67d355ea94fca2bfc6ecdf79f43138779aa0
ArmandasRokas <armandas.rokas@gmail.com>
2018-10-01 19:19:10 +0200

    Update BilletautomatTestAflevering.java

--- test/BilletautomatTestAflevering.java
+++ test/BilletautomatTestAflevering.java
@@ -11,7 +11,6 @@ class BilletautomatTestAflevering {
      * Forventet: Retur penge er lige med nuværende balance.
      *            Efter operationen er balancen er lige med nul
      */
-
     @Test
     void testReturPenge(){
 
@@ -29,4 +28,28 @@ class BilletautomatTestAflevering {
         assertEquals(0, billetautomat.getBalance()); // balance skal være lige med nul efter beløb er udbetalt
 
     }
+      
+     /**
+     * Beskrivelse: Der testes, om det kan ændres billet prisen i montør tilstand.
+     * Forventet: Billetprisen er ændret til en ny pris, som er større 20kr end tidligere pris i montør tilstand
+     */
+    @Test
+    void testSetBilletPrisIMontørTilsand(){
+
+        //Arrange
+        if (!billetautomat.erMontør()){
+            billetautomat.montørLogin("1234");
+        }
+        int nuvaerendeBilletPris = billetautomat.getBilletpris();
+        int nyBilletPris = nuvaerendeBilletPris + 20;
+
+        //Act
+        billetautomat.setBilletpris(nyBilletPris); // sætter en ny billet pris
+
+        //Assert
+        assertEquals(nyBilletPris, billetautomat.getBilletpris()); // Den nye billet pris skal være lige med billetautomatens billet pris.
+
+    }
+
+
 }
