d5f164e183b03bc402c99c1767b99a6ba9fd399d
ArmandasRokas <armandas.rokas@gmail.com>
2018-10-01 18:37:24 +0200

    Update BilletautomatTestAflevering.java

--- test/BilletautomatTestAflevering.java
+++ test/BilletautomatTestAflevering.java
@@ -1,5 +1,33 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 class BilletautomatTestAflevering {
-//Remove this
+
+      Billetautomat billetautomat = new Billetautomat();
+
+    /**
+     * Beskrivelse: Metoden tester, om der kan fås retur penge.
+     *              Det kræver, at balancen ikke er lige med nul
+     *              for at få retur penge.
+     * Forventet: Retur penge er lige med nuværende balance.
+     *            Efter operationen er balancen er lige med nul
+     */
+
+    @Test
+    void testReturPenge(){
+
+        //Arrange
+        if(billetautomat.getBalance()<= 0){
+            billetautomat.indsætPenge(20);
+        }
+        int balanceFoerReturPenge = billetautomat.getBalance();
+
+        //Act
+
+        int returPenge = billetautomat.returpenge();
+
+        //Assert
+        assertEquals(balanceFoerReturPenge, returPenge); // Retur penge skal være lige med balancen, som var før operationen.
+        assertEquals(0, billetautomat.getBalance()); // balance skal være lige med nul efter beløb er udbetalt
+
+    }
 }
